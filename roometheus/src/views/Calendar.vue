6e323bb3974bb8219030ff80572bc51fac6d564f
jzheadley <headleyjz@vcu.edu>
2020-09-26 22:06:53 -0400

    added calendar view

---
+++ roometheus/src/views/Calendar.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="calendar">
+<iframe src="https://calendar.google.com/calendar/embed?src=parksml%40mymail.vcu.edu&ctz=America%2FNew_York" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+    </div>
+</template>
+<script>
+
+
+export default {
+  name: "Calendar",
+};
+</script>
--- roometheus/src/views/Navigation.vue
+++ roometheus/src/views/Navigation.vue
@@ -9,6 +9,7 @@
         <b-nav-item><router-link to="/">Home</router-link></b-nav-item>
         <b-nav-item><router-link to="/about">About</router-link></b-nav-item>
         <b-nav-item><router-link to="/whiteboard">Whiteboard</router-link></b-nav-item>
+        <b-nav-item><router-link to="/calendar">Calendar</router-link></b-nav-item>
       </b-navbar-nav>
 
     </b-collapse>
